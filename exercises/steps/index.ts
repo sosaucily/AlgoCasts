e5f7151745c853b1c917f3388ff5094d885625c9
Jesse Smith <sosaucily@gmail.com>
2019-02-07 11:24:59 -0800

    add new solution for steps; better es6 compilation for ts

---
+++ exercises/steps/index.ts
@@ -0,0 +1,43 @@
+// --- Directions
+// Write a function that accepts a positive number N.
+// The function should console log a step shape
+// with N levels using the # character.  Make sure the
+// step has spaces on the right hand side!
+// --- Examples
+//   steps(2)
+//       '# '
+//       '##'
+//   steps(3)
+//       '#  '
+//       '## '
+//       '###'
+//   steps(4)
+//       '#   '
+//       '##  '
+//       '### '
+//       '####'
+
+//Solution 1:
+// function steps(n: number): void {
+//   let outline: string = "";
+
+//   for (let i = 1; i <= n; i++) {
+//     for (let j = 0; j < i; j++) {
+//       outline = outline.concat("#");
+//     }
+//     for (let j = 0; j < n - i; j++) {
+//       outline = outline.concat(" ");
+//     }
+//     console.log(outline);
+//     outline = "";
+//   }
+// }
+
+//Solution 2
+function steps(n: number): void {
+  for (let i = 1; i <= n; i++) {
+    console.log("#".repeat(i).concat(" ".repeat(n - i)));
+  }
+}
+
+export default steps;
